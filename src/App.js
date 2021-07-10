7aa3c320827ebebb6c977118c6e16aa576eece71
kevinsouza <kevin@piercloud.com.br>
2021-07-09 22:22:16 -0300

    feature: adding layout grid and sidebar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { ThemeProvider, createTheme, makeStyles } from '@material-ui/core';
+import { useState } from 'react';
 import { Home } from './Home';
 
 const useStyles = makeStyles({
@@ -7,21 +8,28 @@ const useStyles = makeStyles({
 
 function App() {
   const classes = useStyles();
+  const [darkMode, setDarkMode] = useState(false);
 
   const theme = createTheme({
     palette: {
+      type: darkMode ? 'dark' : 'light',
       primary: {
         main: '#f44336',
       }, 
       secondary: {
         main: '#3f51b5'
+      },
+      background: {
+        default: darkMode ? '#232323' : '#FFF',
+        dark: darkMode ? '#232323' : '#f4f6f8',
+        paper: darkMode ? '#232323' : '#FFF'
       }
     }
   })
 
   return (
     <ThemeProvider theme={theme}>
-      <Home />
+      <Home darkMode={darkMode} setDarkMode={setDarkMode}/>
     </ThemeProvider>
   );
 }
